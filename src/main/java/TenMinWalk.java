df3dbd00584ff28206d4254fae1ff843c6c3395e
michaelWlodarczak <michael.wlodarczak@gmail.com>
2021-11-17 07:18:35 +0100

    3rd commit. TenMinWalk in progress

--- src/main/java/TenMinWalk.java
+++ src/main/java/TenMinWalk.java
@@ -17,69 +17,101 @@ It will never give you an empty array (that's not a walk, that's standing still!
 import java.util.*;
 
 public class TenMinWalk {
-
     public static boolean isValid(char[] walk) {
-        // Insert brilliant code here
-        return true;
-    }
-
-    public static <N> void main(String[] args) {
-//        int [] arrayN = {1,0};
-//        int [] arrayS = {-1,0};
-//        int [] arrayW = {0,-1};
-//        int [] arrayE = {0,1};
-//
-//        Map<int[],Integer> N = new HashMap<>();
-//        Map<int[],Integer> S = new HashMap<>();
-//        Map<int[],Integer> W = new HashMap<>();
-//        Map<int[],Integer> E = new HashMap<>();
-//
-//        N.put(arrayN,1);
-//        S.put(arrayS,1);
-//        W.put(arrayW,1);
-//        E.put(arrayE,1);
-
         int value = 0;
         int time = 0;
-        char n = 'n';
-        char s = 's';
-        char w = 'w';
-        char e = 'e';
+        int[] arrayN = {1, 0};
+        int[] arrayS = {-1, 0};
+        int[] arrayW = {0, -1};
+        int[] arrayE = {0, 1};
 
-        switch (value){
-            case 'n':
-                int [] arrayN = {1,0};
-                value = Arrays.stream(arrayN).sum();
-                time = 1;
-                break;
-            case 's':
-                int [] arrayS = {-1,0};
-                value = Arrays.stream(arrayS).sum();
-                time = 1;
-                break;
-            case 'w':
-                int [] arrayW = {0,-1};
-                value = Arrays.stream(arrayW).sum();
-                time = 1;
-                break;
-            case 'e':
-                int [] arrayE = {0,1};
-                value = Arrays.stream(arrayE).sum();
-                time = 1;
-                break;
+        for (int i = 0; i < walk.length; i++) {
+            if (walk[i] == 'n') {
+                time++;
+                int valueN = Arrays.stream(arrayN).sum();
+                value += valueN;
+            } else if (walk[i] == 's') {
+                time++;
+                int valueS = Arrays.stream(arrayS).sum();
+                value += valueS;
+            } else if (walk[i] == 'w') {
+                time++;
+                int valueW = Arrays.stream(arrayW).sum();
+                value += valueW;
+            } else if (walk[i] == 'e') {
+                time++;
+                int valueE = Arrays.stream(arrayE).sum();
+                value += valueE;
+            }
         }
+        if (value == 0 && time == 10) {
+            return true;
+        }else {
+            return false;
+        }
+    }
 
 
+    public static void main(String[] args) {
+//        int[] arrayN = {1, 0}; // 5
+//        int[] arrayS = {-1, 0}; //-1
+//        int[] arrayW = {0, -1}; // 3
+//        int[] arrayE = {0, 1}; // 1
+//
+//        System.out.println("arrayN: " + Arrays.stream(arrayN).sum());
+//        System.out.println("arrayS: " + Arrays.stream(arrayS).sum());
+//        System.out.println("arrayW: " + Arrays.stream(arrayW).sum());
+//        System.out.println("arrayE: " + Arrays.stream(arrayE).sum());
+//
+//        int[] arrays = {Arrays.stream(arrayN).sum(), Arrays.stream(arrayS).sum(), Arrays.stream(arrayW).sum(), Arrays.stream(arrayE).sum()};
+//        System.out.println("Total sum: " + Arrays.stream(arrays).sum() + "\n");
+//
+//        char[] walk = {'n', 's', 'w', 'e'};
+//        int value = 0;
+//        int time = 0;
+//
+//        for (int i = 0; i < walk.length; i++) {
+//            if (walk[i] == 'n') {
+//                time++;
+//                int valueN = Arrays.stream(arrayN).sum();
+//                value += valueN;
+//            } else if (walk[i] == 's') {
+//                time++;
+//                int valueS = Arrays.stream(arrayS).sum();
+//                value += valueS;
+//            } else if (walk[i] == 'w') {
+//                time++;
+//                int valueW = Arrays.stream(arrayW).sum();
+//                value += valueW;
+//            } else if (walk[i] == 'e') {
+//                time++;
+//                int valueE = Arrays.stream(arrayE).sum();
+//                value += valueE;
+//            }
+//        }
+//        System.out.println("Value: " + value);
+//        System.out.println("Time: " + time);
 
 
-//        System.out.println(N.values());
-//        System.out.println(S.values());
-//        System.out.println(W.values());
-//        System.out.println(E.values());
+//        int [] arr = {1,2,3};
+//        int sum = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//            sum += arr[i];
+//            //sum = sum + arr[i];
+//        }
+//        System.out.println(sum);
 
-//        System.out.println(Arrays.stream(arrayN).sum());
+//        if (value == 0 && time == 10) {
+//            System.out.println(true);
+//        }else {
+//            System.out.println(false);
+//        }
 
-
-
+        TenMinWalk tenMinWalk = new TenMinWalk();
+        System.out.println(TenMinWalk.isValid(new char[]{'n', 's', 'w', 'e'}));
+        System.out.println(TenMinWalk.isValid(new char[]{'n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'}));
+        System.out.println(TenMinWalk.isValid(new char[]{'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e'}));
+        System.out.println(TenMinWalk.isValid(new char[]{'w'}));
+        System.out.println(TenMinWalk.isValid(new char[]{'n', 'n', 'n', 's', 'n', 's', 'n', 's', 'n', 's'}));
     }
 }

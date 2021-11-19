8e3a31380967059ff9d908e1bee44c25fb1cfa4f
michaelWlodarczak <michael.wlodarczak@gmail.com>
2021-11-19 06:26:49 +0100

    5th commit. Isogram completed

---
+++ src/test/java/JadenCaseTest.java
@@ -0,0 +1,24 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class JadenCaseTest extends TestCase {
+    JadenCase jadenCase = new JadenCase();
+
+    @Test
+    public void test() {
+        assertEquals("toJadenCase doesn't return a valide JadenCase String! try again please :)", "Most Trees Are Blue", jadenCase.toJadenCase("most trees are blue"));
+    }
+
+    @Test
+    public void testNullArg() {
+        assertNull("Must return null when the arg is null", jadenCase.toJadenCase(null));
+    }
+
+    @Test
+    public void testEmptyArg() {
+        assertNull("Must return null when the arg is empty string", jadenCase.toJadenCase(""));
+    }
+
+}
+
+
